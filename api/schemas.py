39d93a4e93aa0e54c8c8762cd4ba83601f14a287
Kevin García <159200143+kngarcia@users.noreply.github.com>
2026-03-09 14:20:28 -0500

    Delete api directory

--- api/schemas.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-from typing import List
-
-class PredictionRequest(BaseModel):
-    texts: List[str]
-
-class PredictionResponse(BaseModel):
-    predictions: List[int]
